3ac0b1c4377a1f01ad9a11898c7dfdaf5be4d7de
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-03-19 16:14:45 -0300

    chore(eslint): use @hitechline config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,91 +1,17 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['import', 'prettier', 'import-helpers', '@typescript-eslint'],
+  plugins: ['import-helpers'],
   extends: [
-    'airbnb',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'plugin:jsx-a11y/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:@typescript-eslint/recommended',
+    '@hitechline/eslint-config/web',
+    '@hitechline/eslint-config/typescript',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  env: {
-    es6: true,
-    jest: true,
-    browser: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  settings: {
-    'import/extensions': ['.js', '.ts', '.tsx'],
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.js', '.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-      },
-    },
-  },
   rules: {
-    'prettier/prettier': 'error',
-
-    'no-shadow': 'off',
-    'no-use-before-define': 'off',
-
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-
-    'import/prefer-default-export': 'off',
-
-    'react/jsx-filename-extension': [
-      'warn',
-      {
-        extensions: ['js', '.jsx', '.tsx'],
-      },
-    ],
-
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-
+    'import/order': 'off',
     'import-helpers/order-imports': [
       'warn',
       {
         newlinesBetween: 'always',
         alphabetize: { order: 'asc', ignoreCase: true },
         groups: ['module', ['parent', 'sibling', 'index']],
-      },
-    ],
-
-    '@typescript-eslint/no-shadow': ['error'],
-    '@typescript-eslint/no-empty-interface': [
-      'error',
-      {
-        allowSingleExtends: true,
-      },
-    ],
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-      {
-        variables: false,
-        functions: false,
-        ignoreTypeReferences: true,
       },
     ],
   },
